c44e08c99f281efe4ba1be681a1a7ec44abab946
Michel BUI <michel71272@gmail.com>
2024-12-04 15:21:02 +0100

    Mise à jour du PUML v2

--- Labyrinth/view/GameWindow.java
+++ Labyrinth/view/GameWindow.java
@@ -1,10 +1,55 @@
 package view;
+import controller.GameController;
 import model.GameObserver;
+import model.TileTemplate;
 
 import javax.swing.*; //peut-être spécifier seulement les composants utilisés
+import java.awt.*;
+import java.util.List;
 
-public class GameWindow implements GameObserver {
-    public GameWindow() {}
+public class GameWindow extends JFrame implements GameObserver {
+    public GameWindow(GameController controller) {
+        super("The Labyrinth");
+        setSize( 500, 500 );
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        JPanel BoardPanel = new JPanel();
+        BoardPanel.setLayout( new GridLayout(7, 7) );
+
+        ImageIcon tuileAngle = new ImageIcon("/img/exempleTuiles/tuile_angle.png");
+        ImageIcon tuileLine = new ImageIcon("/img/exempleTuiles/tuile_line.png");
+        ImageIcon tuileIntersection = new ImageIcon("/img/exempleTuiles/tuile_T.png");
+
+
+        for (int i = 0; i < 49; i++) {
+            JPanel cellPanel = new JPanel();
+            cellPanel.setLayout(new OverlayLayout(cellPanel));
+
+            JLabel tuileLabel = new JLabel();
+
+            JLabel personnageLabel = new JLabel();
+
+            // Ajouter les labels dans le panel de la cellule
+            cellPanel.add(personnageLabel); // Ajouté après pour être superposé
+            cellPanel.add(tuileLabel);
+
+            // Ajouter la cellule dans la grille
+            BoardPanel.add(cellPanel);
+        }
+
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        panel.add( button1, constraints );
+        constraints.gridx = 1;
+        panel.add( button2, constraints );
+        constraints.gridx = 2;
+        panel.add( button3, constraints );
+
+        setVisible(true);
+
+    }
 
     @Override
     public void updateGameBoard() {
