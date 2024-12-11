fe7251949edcdf4d32eb83327e1d43ffbd021457
Daniel YANG <daniel.yang@etu.unistra.fr>
2024-12-11 15:02:55 +0100

    correction et simplification init_board

--- Labyrinth/model/GameBoard.java
+++ Labyrinth/model/GameBoard.java
@@ -1,8 +1,9 @@
 package model;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
+import java.util.ArrayList;
+import java.util.Collections;
+
 
 public class GameBoard {
     private List<List<TileTemplate>> _boardTiles;
@@ -18,54 +19,75 @@ public class GameBoard {
     }
 
     public void init_board(TileFactory tileFactory){
-        Random random = new Random();
 
-        int movableCorners = 16;
-        int movableHallways = 12;
-        int movableIntersections = 6;
+        List<TileTemplate> tiles = new ArrayList<>(34);
+
+        for(int i = 0; i<16; i++)
+            tiles.add(tileFactory.createCorner());
+        for (int i = 0; i < 12; i++)
+            tiles.add(tileFactory.createHallway());
+        for (int i = 0; i < 6; i++)
+            tiles.add(tileFactory.createIntersection());
+
+        Collections.shuffle(tiles);
 
         this._boardTiles.get(0).set(0, tileFactory.createStartingTile(Direction.East)); // Coin fixe 1
         this._boardTiles.get(0).set(6, tileFactory.createStartingTile(Direction.South)); // Coin fixe 2
         this._boardTiles.get(6).set(0, tileFactory.createStartingTile(Direction.North)); // Coin fixe 3
         this._boardTiles.get(6).set(6, tileFactory.createStartingTile(Direction.West)); // Coin fixe 4
 
-        for (int y = 0; y < 7; y++) {
-            for (int x = 0; x < 7; x++) {
-                if (!(y == 0 && x == 0) && !(y == 0 && x == 6) && !(y == 6 && x == 0) && !(y == 6 && x == 6)) {
+        for (int y = 0; y < 7; y++) { //chaque ligne
+            for (int x = 0; x < 7; x++) { //7 colonnes d'une ligne
+                if (!(y == 0 && x == 0) && !(y == 0 && x == 6) && !(y == 6 && x == 0) && !(y == 6 && x == 6)) { // si différent des coins
 
-                    if ((y % 2 != 0 && x % 2 == 0) || (y % 2 != 0 && x % 2 != 0)) {
-                        this._boardTiles.get(y).set(x, tileFactory.createIntersection());
+                    if (y % 2 == 0 && x % 2 == 0) { //si paire-paire → intersection
+                        if(y == 0) //si sur le bord gauche
+                            this._boardTiles.get(y).set(x, tileFactory.createStartingTile(Direction.North));
 
-                    } else {
-                        int tileType = random.nextInt(3); // nombre random entre 0 et 2
-                        if (tileType == 0 && movableCorners > 0) {
-                            this._boardTiles.get(y).set(x, tileFactory.createCorner());
-                            movableCorners--;
-                        }
+                        else if(y == 6) //si sur le bord droit
+                            this._boardTiles.get(y).set(x, tileFactory.createStartingTile(Direction.South));
 
-                        else if (tileType == 1 && movableHallways > 0) {
-                            this._boardTiles.get(y).set(x, tileFactory.createHallway());
-                            movableHallways--;
-                        }
+                        else if(x == 0) //si sur le bord haut
+                            this._boardTiles.get(y).set(x, tileFactory.createStartingTile(Direction.East));
 
-                        else if (tileType == 2 && movableIntersections > 0) {
+                        else if(x == 6) //si sur le bord bas
+                            this._boardTiles.get(y).set(x, tileFactory.createStartingTile(Direction.West));
+
+                        else //si au milieu (direction aléatoire)
                             this._boardTiles.get(y).set(x, tileFactory.createIntersection());
-                            movableIntersections--;
-                        }
-
-                        else {
-                        // Si jamais on peut plus faire en mode random, car il n'y en a aucune de disponible
-                            if (movableCorners > 0) {
-                                this._boardTiles.get(y).set(x, tileFactory.createCorner());
-                                movableCorners--;
-                            } else if (movableHallways > 0) {
-                                this._boardTiles.get(y).set(x, tileFactory.createHallway());
-                                movableHallways--;
-                            } else if (movableIntersections > 0) {
-                                this._boardTiles.get(y).set(x, tileFactory.createIntersection());
-                                movableIntersections--;
-                            }
-                        }
+                    }
+                    else { // sinon autre
+                        this._boardTiles.get(y).set(x, tiles.getFirst());
+                        tiles.removeFirst();
+//                        int tileType = random.nextInt(3); // nombre random entre 0 et 2
+//                        if (tileType == 0 && movableCorners > 0) {
+//                            this._boardTiles.get(y).set(x, tileFactory.createCorner());
+//                            movableCorners--; //décrémentation du type disponible
+//                        }
+//
+//                        else if (tileType == 1 && movableHallways > 0) {
+//                            this._boardTiles.get(y).set(x, tileFactory.createHallway());
+//                            movableHallways--;
+//                        }
+//
+//                        else if (tileType == 2 && movableIntersections > 0) {
+//                            this._boardTiles.get(y).set(x, tileFactory.createIntersection());
+//                            movableIntersections--;
+//                        }
+//
+//                        else {
+//                        // Si jamais on peut plus faire en mode random, car il n'y en a aucune de disponible
+//                            if (movableCorners > 0) {
+//                                this._boardTiles.get(y).set(x, tileFactory.createCorner());
+//                                movableCorners--;
+//                            } else if (movableHallways > 0) {
+//                                this._boardTiles.get(y).set(x, tileFactory.createHallway());
+//                                movableHallways--;
+//                            } else if (movableIntersections > 0) {
+//                                this._boardTiles.get(y).set(x, tileFactory.createIntersection());
+//                                movableIntersections--;
+//                            }
+//                        }
                     }
                 }
             }
